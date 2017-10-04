3f896f5e51568713b4b51e1fc5c0138db6f2670c
Jqu1nteroT <juanpabloquintero25@gmail.com>
2017-10-04 16:38:25 +0200

    deja ca commence le back-end de notre site avec une structure MVC

---
+++ controller/access.php
@@ -0,0 +1,7 @@
+<?php 
+
+require_once 'model/access.php';
+require_once 'view/access.php';
+
+
+?>
---
+++ view/editor.php
@@ -0,0 +1,8 @@
+<h1>Funciono</h1>
+
+<form method="POST" action="index.php?blog=editor" id="msg">
+	<input type="text" name="titulo" placeholder="Identifiant"><br>
+	<input type="text" name="subtitulo" placeholder="Identifiant"><br>
+	<textarea id="usermsg" name="usermsg" rows="1" required></textarea>
+	<button type="button" form="msg">Send</button>
+</form>
